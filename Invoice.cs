48383ba85e0e025acf4b9bff02e17d2c3a79b7e9
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-01-14 19:37:06 +0000

    Add files via upload
    C# 6 for Programmers, Sixth Edition by Paul  & Harvey Deitel, Exercise 4.10 - 5 weeks into learning C#

---
+++ Invoice.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+class Invoice
+{
+
+    public string name;
+
+    public string partNumber;
+
+    public string description;
+
+    public int quantity;
+
+    public decimal pricePerItem;
+
+    
+
+
+    public void SetName(string invoiceName)
+    {
+        name = invoiceName;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+
+
+    public void SetPartNumber(string invoicePartNumber)
+    {
+        partNumber = invoicePartNumber;
+    }
+
+    public string GetPartNumber()
+    {
+        return partNumber;
+    }
+
+
+
+    public void SetDescription(string invoiceDescription)
+    {
+        description = invoiceDescription;
+    }
+
+    public string GetDescription()
+    {
+        return description;
+    }
+
+
+
+    public void SetQuantity(int invoiceQuantity)
+    {
+
+        quantity = invoiceQuantity;
+        
+    }
+
+    public int GetQuantity()
+    {
+        return quantity;
+    }
+
+
+
+    public void SetPricePerItem(decimal invoicePricePerItem)
+    {
+        pricePerItem = invoicePricePerItem;
+    }
+
+    public decimal GetPricePreItem()
+    {
+        return pricePerItem;
+    }
+
+
+
+}
+
+
+
---
+++ InvoiceTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class InvoiceTest
+{
+    static void Main()
+    {
+        Invoice invoice = new Invoice();
+
+        
+
+        Console.WriteLine("Enter name on the invoice: ");
+        string invoicetheName = Console.ReadLine();
+        invoice.SetName(invoicetheName);
+
+
+        Console.WriteLine("Enter the part number: ");
+        string invoicethePartNumber = Console.ReadLine();
+        invoice.SetPartNumber(invoicethePartNumber);
+
+
+        Console.WriteLine("Enter the description: ");
+        string invoicetheDescription = Console.ReadLine();
+        invoice.SetDescription(invoicetheDescription);
+
+
+        Console.WriteLine("Enter the quantity: ");
+        int invoicetheQuantity = int.Parse(Console.ReadLine());
+        invoice.SetQuantity(invoicetheQuantity);
+
+
+        Console.WriteLine("Enter the price: ");
+        decimal invoicethePicePerItem = decimal.Parse(Console.ReadLine());
+        invoice.SetPricePerItem(invoicethePicePerItem);
+
+        Console.WriteLine($"\nCustomer: {invoice.GetName()}");
+        Console.WriteLine($"\nPart number: {invoice.GetPartNumber()}");
+        Console.WriteLine($"\nDescription: {invoice.GetDescription()}");
+        Console.WriteLine($"\nQuantity: {invoice.GetQuantity()}");
+        Console.WriteLine($"\nPrice: {invoice.GetPricePreItem()}");
+        Console.WriteLine($"\nTotal: {invoice.GetQuantity() * invoice.GetPricePreItem()}");
+
+
+    }
+    
+}
